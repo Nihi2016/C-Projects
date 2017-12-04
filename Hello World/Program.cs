8bc25cd5ab3ae6dceaddbe0a54d434a88cbccb5f
Babitha <S524976@mail.nwmissouri.edu>
2017-12-04 15:48:15 -0600

    Enter name from the console and display it with current date and time

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -6,8 +6,11 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Press any key to continue...");
+            Console.WriteLine("\n What is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\n Hello , {name}, on {date:d} at {date:t}");
+            Console.Write("\n Press any key to exit..");
             Console.ReadKey(true);
         }
     }
